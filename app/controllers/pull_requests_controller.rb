54a94f8f29adb6d03d3f59358692bb266cefae9e
Joey Butler <Joey.butler@livingsocial.com>
2012-06-25 23:06:39 -0400

    remove old controller

--- app/controllers/pull_requests_controller.rb
+++
@@ -1,6 +0,0 @@
-class PullRequestsController < ApplicationController
-  def index
-    aggregate = Github::PullRequestAggregate.new
-    render json: aggregate.all_pulls_across_all_repos_for_user('mattmueller')
-  end
-end
