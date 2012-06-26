aef198ee2f5ea3a395bf936520df306995fe8401
Joey Butler <Joey.butler@livingsocial.com>
2012-06-25 23:16:44 -0400

    rename the aggregate and correct the db config

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,6 +1,6 @@
 class IssuesController < ApplicationController
   def index
-    aggregate = Github::PullRequestAggregate.new
-    @repos = aggregate.all_pulls_across_all_repos_for_user('defunkt')
+    aggregate = Github::IssueAggregate.new
+    @repos = aggregate.for_user('defunkt')
   end
 end
---
+++ lib/github/issue_aggregate.rb
@@ -0,0 +1,16 @@
+module Github
+  class IssueAggregate
+    def initialize(pr_finder=nil)
+      @pr_finder = pr_finder || lambda { ::Github::Gateway.new }.call
+    end
+
+    # @return [Repo] Returns repos, each with a reference to the pull requests.
+    def for_user(username)
+      repos = @pr_finder.find_repos_for_user(username)
+      repos.map do |repo|
+        repo.pulls = @pr_finder.find_pulls_for_repo(repo.full_name)
+        repo
+      end
+    end
+  end
+end
