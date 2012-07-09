c276cb09f64ea26a453b3b310f74ac37df95c0ee
Joey Butler <Joey.butler@livingsocial.com>
2012-07-09 00:47:42 -0400

    initial commit

--- config/application.rb
+++ config/application.rb
@@ -65,5 +65,6 @@ module Gtpr
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.assets.initialize_on_precompile = false
+    config.force_ssl = true
   end
 end
