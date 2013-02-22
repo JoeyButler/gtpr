4296104cfd754498c731ae1a8453ce83c25386b2
Joey Butler <Joey.butler@livingsocial.com>
2013-02-22 02:16:35 -0500

    Add field for selecting a user.

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -1,7 +1,8 @@
 class ReposController < ApplicationController
   def index
     aggregate = Github::IssueAggregate.new(@gh_client)
-    @repos = aggregate.for_user 'mattmueller'#(current_user.name)
+    username = params.fetch(:username, current_user.name)
+    @repos = aggregate.for_user(username)
     # Do we need to show repos without any issues?
     # @repos.reject! {|r| r.open_issues.zero? }
     @repos.reject! {|r| r.private }
